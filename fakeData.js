a90ec91db90d7ea4ca87e6b33f88c2af67e2edee
Damodar Hegde <dhegde50@gmail.com>
2023-03-17 15:50:33 -0500

    Added data file

---
+++ fakeData.js
@@ -0,0 +1,39 @@
+const userList = [
+  {
+    id: 1,
+    name: 'Damodar Hegde',
+    username: 'damodarh',
+    age: 26,
+    nationality: 'Indian',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    username: 'janeSmith',
+    age: 22,
+    nationality: 'European',
+  },
+  {
+    id: 3,
+    name: 'Sage',
+    username: 'sage123',
+    age: 32,
+    nationality: 'Chinese',
+  },
+  {
+    id: 4,
+    name: 'Kill Joy',
+    username: 'kJoy',
+    age: 21,
+    nationality: 'German',
+  },
+  {
+    id: 5,
+    name: 'Chamber',
+    username: 'chamber123',
+    age: 30,
+    nationality: 'Japanese',
+  },
+];
+
+module.exports = { userList };
