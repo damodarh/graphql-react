500bb61a7636d5f5e5d2ad30a79ef8cf6027f9f8
Damodar Hegde <dhegde50@gmail.com>
2023-03-19 00:01:27 -0500

    Added query types for reference

--- server/fakeData.js
+++ server/fakeData.js
@@ -46,25 +46,124 @@ const userList = [
 ];
 
 const movieList = [
-    {
-        id: 1,
-        name: 'Inception',
-        yearOfPublication: 2010,
-        isInTheatres: false
-    },
-    {
-        id: 2,
-        name: 'Interstellar',
-        yearOfPublication: 2014,
-        isInTheatres: true
-    },
-    {
-        id: 3,
-        name: 'Avengers Endgame',
-        yearOfPublication: 2019,
-        isInTheatres: true
-    },
-
-]
+  {
+    id: 1,
+    name: 'Inception',
+    yearOfPublication: 2010,
+    isInTheatres: false,
+  },
+  {
+    id: 2,
+    name: 'Interstellar',
+    yearOfPublication: 2014,
+    isInTheatres: true,
+  },
+  {
+    id: 3,
+    name: 'Avengers Endgame',
+    yearOfPublication: 2019,
+    isInTheatres: true,
+  },
+];
 
 module.exports = { userList, movieList };
+
+/*
+Sample Queries
+
+query getUsers {
+  users {
+    id,
+    name,
+    age,
+    nationality,
+    username,
+    friends {
+      age,
+      name
+    }
+  }
+}
+# query getUser($userId: ID!){
+#   user(id: $userId){
+#     name
+#     age
+#     nationality
+#     friends{
+#       name
+#     }
+#   }
+# }
+
+# query allMovies{
+#   movies{
+#     name,
+#     yearOfPublication,
+#     isInTheatres
+#   }
+# }
+
+# query getMovie($name: String!){
+#   movie(name: $name) {
+#     name
+#     yearOfPublication
+#     isInTheatres
+#   }
+# }
+
+# query getUserMovieMix($userId: ID!){
+#   user(id: $userId) {
+#     name
+#     age
+#     nationality
+#     favoriteMovies {
+#       name
+#       yearOfPublication
+#       isInTheatres
+#     }
+#   }  
+# }
+
+# mutation($input: CreateUserInput!){
+#   createUser(input: $input) {
+#     name
+#     age
+#     nationality
+#   }
+# }
+
+# mutation($input: updateUsernameInput!){
+#   updateUsername(input: $input) {
+#     id
+#     username
+#   }
+# }
+
+# mutation($deleteUserId: ID!){
+#  deleteUser(id: $deleteUserId) {
+#    name
+#    id
+#    username
+#  }
+# }
+
+Sample Data for testing
+// {
+//   "input": {
+//     "id": "6",
+//     "newUsername": "testUser-1"
+//   },
+//   "deleteUserId": null
+// }
+// {
+//   "input":{
+//     "name": "Test User",
+//     "age": 25,
+//     "username": "testUser"
+//   }
+// }
+// {
+//   "deleteUserId": "6"
+// }
+
+*/
